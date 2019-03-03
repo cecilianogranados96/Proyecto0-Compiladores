a9512c337e0da78c0e0aa203c579932183305237
José Andrés Ceciliano Granados <cecilianogranados96@hotmail.com>
2019-03-03 17:22:45 -0600

    [Correccion ejemplo 2]

--- semantic.c
+++ semantic.c
@@ -40,54 +40,12 @@ void read_id(REG_EXPRESION in)
     /* Genera la instruccion para leer */
     generate("Read", in.name, "Integer", "");
     
-    //**********ENSAMBLADOR**********************************************************************
-    char *message = "; ------------------------  \n \
-                    \t; Leer de consola -> read \n \
-                    ; \n \n \
-                    mov eax, 3 	; EAX -> read  \n \
-                    mov ebx, 0 	; EBX -> input \n \
-                    mov ecx, buffer_input ; ECX -> valor en que se almacena   \n \
-                    mov edx, len_input	; EDX -> largo de lo que voy a leer \n \
-                    int 0x80 	; llamada al sistema operativo    \n  \n \
-                    mov edi, eax \n \
-                    xor edx, edx \n \
-                    xor esi, esi \n \
-                    xor eax, eax \n \
-                    mov ebx, 10  \n \
-                                 \n \
-                    dec edi		 \n \
-                    call atoi   \n \
-                    mov [esp + %s], ax \n \
-                    ; Realiza el read -> y se almacena en memoria. \n \
-                    ; ---------------------------  \n \n ";
-	fprintf(out_ensambler, message, in.name);
-    //**********ENSAMBLADOR**********************************************************************
-    
 }
 
 void write_expr(REG_EXPRESION out)
 {
     /* Genera la instruccion para escribir */
     generate("Write",  extract(&out), "Integer", "");
-    
-    //**********ENSAMBLADOR********************************************************************** 
-    char *message = "; ---------------------------  \n \
-                        \t; Escribir en consola -> write \n \
-                        ; \n \
-                        mov ax, [esp + %s]  ; pongo en el ax e\n \
-                        mov ebx, 10 \n \
-                        mov esi, 30 \n \
-                        call itoa   \n \
-                        \n \
-                        mov eax, 4 	; EAX -> write  \n \
-                        mov ebx, 1 	; EBX -> input  \n \
-                        mov ecx, buffer_output ; ECX -> valor que se va a imprimir     \n \
-                        mov edx, len_output	; EDX -> largo de lo que voy a escribir \n \
-                        int 0x80 	; llamada al sistema operativo      \n \
-                        ; ---------------------------  \n \n ";
-	fprintf(out_ensambler, message, out.name);
-    //**********ENSAMBLADOR**********************************************************************
-    
     
     
 }
@@ -125,47 +83,12 @@ REG_EXPRESION gen_infix(REG_EXPRESION e1, char *op, REG_EXPRESION e2)
                 strcpy(arr, "add");
             }
             sprintf(cadTemp, "%d", e_rec.value);
-            
-            //**********ENSAMBLADOR**********************************************************************
-            char *messageTwoLi = " ; ---------------------------  \n \
-                                    \t; %s Literal, result \n \
-                                    mov eax, %s        \n \
-                                    mov [esp + %s], ax   \n \
-                                    ; ---------------------------  \n \n ";
-            fprintf(out_ensambler, messageTwoLi, arr, e_rec.name, cadTemp);
-            //**********ENSAMBLADOR**********************************************************************
-            
             strcpy(e_rec.name, cadTemp);
             return e_rec;
 	}else{
-    
         check_id(cadTemp);
         generate(cadOp, e1.name,  e2.name, cadTemp);
-        strcpy(reg.name, cadTemp);
-     
-        //**********ENSAMBLADOR**********************************************************************
-        char *message;
-        if (e1.clase == LITERALEXPR || e2.clase == LITERALEXPR){
-		  message = "; ---------------------------  \n \
-                     \t; %s  op1, Literal, result \n \
-                     mov eax, %s \n \
-                     mov ebx, [esp + %s] \n \
-                     %s eax, ebx \n \
-                     mov [esp + %s], ax \n \
-                     ; ---------------------------  \n \n ";
-        }else{
-            message = "; ---------------------------  \n \
-                        \t; %s  op1, op2, result \n \
-                        mov eax, [esp + %s] \n \
-                        mov ebx, [esp + %s] \n \
-                        %s eax, ebx \n \
-                        mov [esp + %s], ax \n \
-                        ; ---------------------------  \n \n ";
-        }  
-        fprintf(out_ensambler, message, op[0], e1.name, e2.name, op[0], e_rec.name);
-        //**********ENSAMBLADOR**********************************************************************
-
-        
+        strcpy(reg.name, cadTemp);   
         return reg;
     }    
 }
